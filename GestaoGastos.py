645bce2fcaae28240c6cefbc37bb8682faf9b91c
duda <duda.fucape@gmail.com>
2025-11-12 13:03:26 -0300

    Prototipo v1, menu

--- GestaoGastos.py
+++ GestaoGastos.py
@@ -1,19 +1,18 @@
-import pandas as pd
 from openpyxl import Workbook
-from Funcoes import funcao
+from GestaoFinanceira.Funcoes import funcao
 
 wb = Workbook()
 ws = wb.active
-ws.title = "Controle de gastos"
+ws.title = "Gestão de transações"
+ws.append(["Tipo", "Categoria", "Valor", "Data"])
 
-ws.append(["Data", "Categoria", "Valor"])
-
-wb.save("desafioGIT.xlsx")
+wb.save("Gestao_Financeira.xlsx")
 
 while True:
     print("\n================= MENU PRINCIPAL =================")
-    print(f"Bem vindo a lista de chamados, selecione o que deseja: \n 1- Adicionar gastos\n 2- Vizualizar gastos\n 0- Encerrar")
-    n = int(input(f"Escolha o que deseja:"))
+    print(f"Bem vindo á sua gestão financeira! Selecione o que deseja: \n 1- Adicionar transação\n 2- Remover transação\n "
+          f"3- Listar Tansação por categoria\n 4- Listar Tansação por período\n 5- Vizualizar o saldo\n 0- Encerrar")
+    n = int(input(f"Escolha a opção que deseja:"))
     resultado = funcao(n, ws, wb)
     if resultado == "quebra":
         break
