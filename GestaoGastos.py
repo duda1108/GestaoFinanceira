20038334eef5b25e891a02478eb12c6593ac82a7
duda <duda.fucape@gmail.com>
2025-11-17 13:10:46 -0300

    menu de controle

--- GestaoGastos.py
+++ GestaoGastos.py
@@ -1,11 +1,16 @@
 from openpyxl import Workbook
-from GestaoFinanceira.Funcoes import funcao
-
+from GestaoFinanceira.Transacoes import transacoes
+from GestaoFinanceira.ListarTransacoes import vizualizar
 wb = Workbook()
 ws = wb.active
-ws.title = "Gestão de transações"
-ws.append(["Tipo", "Categoria", "Valor", "Data"])
+ws.title = "Janeiro"
+meses = ["Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+for m in meses:
+    wb.create_sheet(m)
 
+    # Cabeçalho em todas as planilhas
+for sheet in wb.worksheets:
+    sheet.append(["Tipo","Categoria","Valor","Data","Descrição"])
 wb.save("Gestao_Financeira.xlsx")
 
 while True:
@@ -13,6 +18,9 @@ while True:
     print(f"Bem vindo á sua gestão financeira! Selecione o que deseja: \n 1- Adicionar transação\n 2- Remover transação\n "
           f"3- Listar Tansação por categoria\n 4- Listar Tansação por período\n 5- Vizualizar o saldo\n 0- Encerrar")
     n = int(input(f"Escolha a opção que deseja:"))
-    resultado = funcao(n, ws, wb)
+    resultado = transacoes(n, ws, wb)
+    resultado1 = vizualizar(n, ws, wb)
     if resultado == "quebra":
+        break
+    elif resultado1 == "quebra":
         break
